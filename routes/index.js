62ab4d9617916d37193ce6f79714139433822103
Jeremy M Rivera <fangman09@gmail.com>
2023-04-10 22:43:10 -0400

    coding session 12

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const clothingItem = require("./clothingItem");
+
+router.use("/items", clothingItem);
+
+router.use((req, res) => {
+  res.status(500).send({ message: "router not found" });
+});
+
+module.exports = router;
