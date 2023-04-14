f0d7a22030be23fe715f798faf801f679aa167bb
Jeremy M Rivera <fangman09@gmail.com>
2023-04-14 18:15:14 -0400

    skeleton completed, ready for step 8

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
+const user = require('./user');
+
 const clothingItem = require('./clothingItem');
 
 router.use('/items', clothingItem);
 
+router.use('/users', user);
+
 router.use((req, res) => {
-  res.status(500).send({ message: 'router not found' });
+  res.status(500).send({ message: 'Requested resource not found' });
 });
 
 module.exports = router;
