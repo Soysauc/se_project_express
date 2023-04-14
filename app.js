9727da3f1e5f26780288c2a2cecf9c0ca24e087e
Jeremy M Rivera <fangman09@gmail.com>
2023-04-14 18:59:48 -0400

    try to pass tests

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
-const { PORT = 3001 } = process.env;
-const app = express();
-
-mongoose.connect('mongodb://localhost:27017/wtwr_db');
-
 const routes = require('./routes');
 
-app.use(express.json());
+const { PORT = 3001 } = process.env;
+const app = express();
+mongoose.connect('mongodb://localhost:27017/wtwr_db');
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6439d3bdf7ed51812993350c',
+  };
+  next();
+});
+
+app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
